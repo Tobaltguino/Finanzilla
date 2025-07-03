2c70ca5378e1facdb9951fd6ece52f3d2bc9619a
Jeany-B <jeany.aravena.l@hotmail.com>
2025-07-03 17:37:06 -0400

    Se modifica el archivo settings.py, para que no tenga información sensible sobre el servidor.

--- backend/finanzilla/settings.py
+++ backend/finanzilla/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv() # Cargar archivos .env, donde está información importante para cargar el servidor
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -71,11 +74,14 @@ WSGI_APPLICATION = 'finanzilla.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD':os.getenv('DB_PASSWORD'),
+        'HOST':os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
