f98d1c186d478b011f32e3c721a95b851f9912ca
Jeany-B <jeany.aravena.l@hotmail.com>
2025-07-03 19:58:52 -0400

    Se añade la autenticación de los usuarios mediante una clase personalizada para la base de datos.

--- backend/finanzilla/settings.py
+++ backend/finanzilla/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'login.User'
+
