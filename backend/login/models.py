7aa68e304faa7a7cfe9161c8be66e39c1272707d
Jeany-B <jeany.aravena.l@hotmail.com>
2025-07-03 20:00:06 -0400

    Se comienza con la APP de iniciar sesión. Es lo predefinido de Django, nada nuevo.

---
+++ backend/login/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    DIVISAS = [
+        ('USD', 'Dólar estadounidense'),
+        ('CLP', 'Peso chileno'),
+        ('EUR', 'Euro'),
+    ]
+    divisa = models.CharField(max_length=3, choices=DIVISAS, default='USD')
+
+    def __str__(self):
+        return self.username
+
+class Categoria(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+
+    #notificaciones_activadas = models.BooleanField(default=True)
+    #categoria_favorita = models.CharField(max_length=100, blank=True, null=True)
+
+class Gasto(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=200)
+    fecha = models.DateField()
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
