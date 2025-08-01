d61c684d0a746bf1cc353836e0513f8a9c175d98
Jeany-B <jeany.aravena.l@hotmail.com>
2025-08-01 09:17:58 -0400

    Se actualizó el ENGINE a Postgresql

--- backend/finanzilla/settings.py
+++ backend/finanzilla/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'finanzilla.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD':os.getenv('DB_PASSWORD'),
